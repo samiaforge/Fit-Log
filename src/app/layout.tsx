9d5eec7f7d01bbc9e9d4db733d7f110df414cba6
MehCodez <mehelisamia823@gmail.com>
2026-09-25 17:59:46 +0600

    Complete responsive hero banner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,10 @@ import "./globals.css";
 import Navbar from "@/components/shared/Navbar";
 import Footer from "@/components/shared/Footer";
 
+
+
+
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -25,7 +29,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col ">
         <Navbar/>
         {children}
         <Footer/>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 
+import Banner from "@/components/homepage/Banner";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-     <p>shghdvh</p>
+     <Banner/>
     </div>
   );
 }
